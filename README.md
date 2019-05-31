8079028c137635cec03248296347b58918c7116a
jakubkoje <you@example.com>
2019-05-31 21:08:15 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Smart Brain Api
+This is a backend for Smart Brain application.
+Made with NodeJs, Express and PostgreSQL.
---
+++ controllers/signin.js
@@ -0,0 +1,30 @@
+const handleSignin = (req, res, db, bcrypt) => {
+	const { email, password } = req.body;
+	if (!email || !password) {
+		return res.status(400).json("incorrect form submission");
+	}
+
+	db.select("email", "hash")
+		.where("email", "=", req.body.email)
+		.from("login")
+		.then(data => {
+			const isValid = bcrypt.compareSync(req.body.password, data[0].hash);
+			if (isValid) {
+				return db
+					.select("*")
+					.from("users")
+					.where("email", "=", req.body.email)
+					.then(user => {
+						res.json(user[0]);
+					})
+					.catch(err => res.status(400).json("Unable to get user"));
+			} else {
+				res.status(400).json("wrong credentials");
+			}
+		})
+		.catch(err => res.status(400).json("wrong credentials"));
+};
+
+module.exports = {
+	handleSignin: handleSignin
+};
